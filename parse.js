aa1bbd04b18fceb32a70afda955078789e39bb79
amandayamasaki <amandayamasaki@u.northwestern.edu>
2018-10-10 09:11:05 -0500

    updates

--- parse.js
+++ parse.js
@@ -4,97 +4,126 @@ const fs = require('fs')
 const myArgs = process.argv
 const fileName = process.argv[2]
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
-// tags, variables, trigger
 
-// var tag_audit = createCsvWriter({
-//     path: 'tag_audit.csv',
-//     header: [
-//         // {id: 'name', title: 'NAME'},
-//         // {id: 'lang', title: 'LANGUAGE'}
-//     ]
-// });
- 
-var tags = [
-    // {name: 'Bob',  lang: 'French, English'},
-    // {name: 'Mary', lang: 'English'}
-];
-
-function searchTable(table, toFind){
-    for (var k in table) {
-        // use hasOwnProperty to filter out keys from the Object.prototype
-        if (table.hasOwnProperty(k)) {
-            if (k === toFind){
-                return table[k];
+Object.prototype.searchTable = function(toFind){
+    for (var key in this) {
+        if (this.hasOwnProperty(key)) {
+            if (key === toFind){
+                return this[key];
             }
         }
     }
     return null;
 }
 
-function generateColumn(category, filepath){
-    let sortKeys = function(){
+Array.prototype.pushIfUnique = function(toAdd){
+    if (this.indexOf(toAdd) === -1){
+        this.push(toAdd)
+    }
+}
+
+function csv(category, filepath){
+    this.category = category
+    this.filepath = filepath
+    this.keys = []
+    this.filterKeys = []
+    this.paramKeys = []
+    this.init = function(){
         var keys = []
-        for (let i=0;i<category.length;i++){
-            var newKey = category[i].parameter.map(a => a.key);
-            for (var j=0;j<newKey.length;j++){
-                if (keys.indexOf(newKey[j]) === -1){
-                    keys.push(newKey[j])
-                }
+        var paramKeys = []
+        var filterKeys = []
+        for (let i=0;i<this.category.length;i++){
+            Object.keys(this.category[i]).forEach(function(obj){
+                keys.pushIfUnique(obj);
+            })
+            // checks to see if tag or variable
+            if (this.category[i].searchTable("parameter") !== null){
+                this.category[i].parameter.map(a => a.key).forEach(function(obj){
+                    paramKeys.pushIfUnique(obj);
+                });
+            }
+            // // checks to see if trigger
+            if (this.category[i].searchTable("filter") !== null){
+                this.category[i].filter.forEach(function(child){
+                    Object.keys(child).forEach(function(obj){
+                        filterKeys.pushIfUnique(obj);
+                    })
+                    child.parameter.map(a => a.key).forEach(function(obj){
+                        paramKeys.pushIfUnique(obj);
+                    });
+                })
             }
         }
-        return keys.sort()
+        this.keys = keys.filter(word => word !== "parameter" && word !== "filter")
+        this.filterKeys = filterKeys.filter(word => word !== "parameter")
+        this.paramKeys = paramKeys.sort()
+        return "init done"
     }
-    let audit = {
-        path: filepath,
-        header: []
+    this.generateColumns = function(){
+        var columns = {
+            path: this.filepath,
+            header: []
+        }
+        function iterateOverKeys(colKeys, trig=false){
+            let flag = trig === false ? "" : trig
+            for (let i=0;i<colKeys.length;i++){
+                var k = new Object()
+                k['id'] = colKeys[i] + flag
+                k['title'] = colKeys[i] + flag
+                columns.header.push(k)
+            }
+        }
+        iterateOverKeys(this.keys)
+        iterateOverKeys(this.filterKeys, "|condition")
+        iterateOverKeys(this.paramKeys)
+        return columns
     }
-    for (let i=0;i<sortKeys().length;i++){
-        var k = new Object()
-        k['id'] = sortKeys()[i]
-        k['title'] = sortKeys()[i]
-        audit.header.push(k)
+    this.generateRows = function(){
+        var rows = []
+        for (let i=0;i<this.category.length;i++){
+            var k = new Object()
+            for (let j=0; j<this.keys.length;j++){
+                let res = JSON.stringify(this.category[i].searchTable(this.keys[j])) !== "null" ? JSON.stringify(this.category[i].searchTable(this.keys[j])) : ""
+                k[this.keys[j]] = res.split("\"").join("").split("[").join("").split("]").join("")
+            }
+            if (this.category[i].searchTable("parameter") !== null){
+                this.category[i].searchTable("parameter").forEach(function(obj){
+                    k[obj.searchTable("key")] = obj.searchTable("value")
+                })
+                rows.push(k)
+            }
+            if (this.category[i].searchTable("filter") !== null){
+                this.category[i].searchTable("filter").forEach(function(child){
+                    k["type|condition"] = child.searchTable("type")
+                    child.searchTable("parameter").forEach(function(obj){
+                        k[obj.searchTable("key")] = obj.searchTable("value")
+                    })
+                    rows.push(k)
+                })
+                rows.push(k)
+            }
+            rows.push(k)
+        }
+        return rows
     }
-    return audit
+    this.generateCsv = function(){
+        this.init()
+        var audit = createCsvWriter(this.generateColumns())
+        audit.writeRecords(this.generateRows()).then(() => {
+            console.log('...csv complete');
+        });
+        return
+    }
 }
 
 fs.readFile(fileName, 'utf8', function(err, data) {
 	const container = JSON.parse(data);
-    let tag = container.containerVersion.tag
-    let trigger = container.containerVersion.trigger
-    let variable = container.containerVersion.variable
+    let tag = new csv(container.containerVersion.tag, 'tag_audit.csv')
+    let trigger = new csv(container.containerVersion.trigger, 'trigger_audit.csv')
+    let variable = new csv(container.containerVersion.variable, 'variable_audit.csv')
 
-    var tag_audit = createCsvWriter(generateColumn(tag, 'tag_audit.csv'))
-    tag_audit.writeRecords(tags).then(() => {
-        console.log('...Done');
-    });
+    tag.generateCsv();
+    variable.generateCsv();
+    // trigger.generateCsv();
+
 });
-
- 
-// "trigger": [
-//             {
-//                 "accountId": "27543852",
-//                 "containerId": "845817",
-//                 "triggerId": "167",
-//                 "name": "All US Pages - DOM Ready",
-//                 "type": "DOM_READY",
-//                 "filter": [
-//                     {
-//                         "type": "CONTAINS",
-//                         "parameter": [
-//                             {
-//                                 "type": "TEMPLATE",
-//                                 "key": "arg0",
-//                                 "value": "{{Page Hostname}}"
-//                             },
-//                             {
-//                                 "type": "TEMPLATE",
-//                                 "key": "arg1",
-//                                 "value": "atkins.com"
-//                             }
-//                         ]
-//                     }
-//                 ],
-//                 "fingerprint": "1472743188945"
-//             },
-
-
